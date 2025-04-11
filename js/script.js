a51d50c5f430627d67f91231b5a212917b3fa668
Will Mayrink <will.mayrink@gmail.com>
2025-04-11 00:05:14 -0300

    new functionalities
    collapse; fake qrcode

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showDiv() {
+    var div = document.getElementById("qrcodeDiv");
+    if(div.style.display === "none") {
+        div.style.display = "block";
+    } else{
+        div.style.display = "none";
+    }
+}
